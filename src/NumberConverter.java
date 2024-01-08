b681969e6b13e36b40af969f0a7d530c6b4235ad
unknown <BT_4N2_33@BKTECH.LOCAL>
2024-01-08 14:01:53 -0500

    Initial commit

---
+++ src/NumberConverter.java
@@ -0,0 +1,54 @@
+public class NumberConverter {
+    int[] digits;
+    int base;
+
+    public NumberConverter(int number, int base) {
+        String numberAsString = Integer.toString(number);
+        digits = new int[numberAsString.length()];
+        for (int i = 0; i < numberAsString.length(); i++) {
+            String single = numberAsString.substring(i,i+1);
+            int d = Integer.parseInt(single);
+            digits[i] = d;
+        }
+        this.base = base;
+    }
+
+    public String displayOriginalNumber() {
+        String o = "";
+        for (int i = 0; i < digits.length; i++) {
+            o = o + digits[i];
+        }
+        o = o + "\n";
+        return o;
+    }
+
+    public int[] getDigits() {
+        return digits;
+
+    }
+
+    public int[] convertToDecimal() {
+        int base10 = 0;
+        for (int i = 0; i < digits.length; i++) {
+            base10 += digits[i] * Math.pow(base, digits.length - 1);
+        }
+        String base10AsString = "" + base10;
+
+        int [] base10InArray = new int[base10AsString.length()];
+        for (int i = 0; i < base10AsString.length(); i++) {
+            String singleDigit = base10AsString.substring(i,i+1);
+            int d = Integer.parseInt(singleDigit);
+            base10InArray[i] = d;
+        }
+        return base10InArray;
+    }
+
+    public int[] convertToBinary() {
+        return null;
+    }
+
+    public int[] convertToOctal() {
+        return null;
+    }
+}
+
